ee495a56b5d5ee16ce8acd8d12988eb3bf74bd4a
zqp <345311171@qq.com>
2017-10-27 19:56:58 +0800

    创建实体类,写了两个属性

---
+++ Git_Test/src/entity/Emp.java
@@ -0,0 +1,25 @@
+package entity;
+
+public class Emp {
+
+	
+	private int empno;
+	private String name;
+	
+	
+	public int getEmpno() {
+		return empno;
+	}
+	public void setEmpno(int empno) {
+		this.empno = empno;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+}
